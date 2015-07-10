e86c8eda1f8a0aa9ddeed6b969c83336a6dcf952
Mikhail Bolshakov <veujin@gmail.com>
2015-07-10 04:03:51 +0300

    Added binary tree

--- Bolshakov_1/Set.cs
+++ Bolshakov_1/Set.cs
@@ -6,7 +6,111 @@ using System.Threading.Tasks;
 
 namespace Bolshakov_1
 {
-    public class Set
+    public class Set<T>: ISet<T>
     {
+
+
+        #region ISetMethods
+
+        public bool Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ExceptWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IntersectWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSubsetOf(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsSupersetOf(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Overlaps(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetEquals(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SymmetricExceptWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UnionWith(IEnumerable<T> other)
+        {
+            throw new NotImplementedException();
+        }
+
+        void ICollection<T>.Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Count
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool IsReadOnly
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
     }
 }
---
+++ Search_Tree_Tests/SimpleMethTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bolshakov_1;
+
+namespace Search_Tree_Tests
+{
+    [TestClass]
+    public class SimpleMethTests
+    {
+        [TestMethod]
+        public void ZeroBalanceFactor_1()
+        {
+            var testNode = new BinNode<int>(3);
+            Assert.AreEqual(testNode.BalanceFactor, 0);
+        }
+
+        [TestMethod]
+        public void ZeroBalanceFactor_2()
+        {
+            var testNode = new BinNode<int>(3);
+            testNode.LeftChild = new BinNode<int>(5);
+            testNode.RightChild = new BinNode<int>(7);
+            Assert.AreEqual(testNode.BalanceFactor, 0);
+        }
+    }
+}
