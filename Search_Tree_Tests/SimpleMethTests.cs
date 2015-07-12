db9644b0fb40491aa2a625c97e8b8b27154e80a7
Mikhail Bolshakov <veujin@gmail.com>
2015-07-12 19:38:27 +0300

    New test

--- Search_Tree_Tests/SimpleMethTests.cs
+++ Search_Tree_Tests/SimpleMethTests.cs
@@ -9,160 +9,25 @@ namespace Search_Tree_Tests
     public class SimpleMethTests
     {
         [TestMethod]
-        public void ZeroBalanceFactor_1()
+        public void RndCountTest()
         {
-            var a = 3;
-            var testNode = new BinNode<int>(ref a);
-
-            Assert.AreEqual(0, testNode.BalanceFactor);
-        }
-
-        [TestMethod]
-        public void TreeAddResultTest()
-        {
+            var rnd = new Random();
             var tree = new Tree<int>();
-            tree.Add(3);
-            bool result = tree.Add(3);
-            var expected = false;
+            var added = rnd.Next(100);
+            var deleted = rnd.Next(added);
+            var expect = added - deleted;
 
-            Assert.AreEqual(expected, result);
-        }
+            for (int i = 0; i < added; i++)
+            {
+                tree.Add(i);
+            }
 
-        [TestMethod]
-        public void TreeIntCountTest()
-        {
-            var tree = new Tree<int>();
-            tree.Add(3);
-            tree.Add(4);
-            var expected = 2;
+            for (int i = 0; i < deleted; i++)
+            {
+                tree.Remove(i);
+            }
 
-            Assert.AreEqual(expected, tree.Count);
-        }
-
-        [TestMethod]
-        public void TreeIntDublicateTest2()
-        {
-            var tree = new Tree<int>();
-            tree.Add(3);
-            tree.Add(3);
-            tree.Add(4);
-            tree.Add(4);
-            var expected = 2;
-
-            Assert.AreEqual(expected, tree.Count);
-        }
-
-        [TestMethod]
-        public void TreeIntDublicateTest3()
-        {
-            var tree = new Tree<int>();
-            tree.Add(3);
-            tree.Add(4);
-            tree.Add(5);
-            tree.Add(3);
-            var expected = 3;
-
-            Assert.AreEqual(expected, tree.Count);
-        }
-
-        [TestMethod]
-        public void TreeReftypeDublicateTest1()
-        {
-            var tree = new Tree<List<int>>();
-            var a = new List<int>();
-            var b = new List<int>();
-            var expected = 2;
-
-            tree.Add(a);
-            tree.Add(b);
-            tree.Add(a);
-
-            Assert.AreEqual(expected, tree.Count);
-        }
-
-        [TestMethod]
-        public void TreeReftypeDublicateTest2()
-        {
-            var tree = new Tree<List<int>>();
-            var a = new List<int>();
-            var b = new List<int>();
-            var expected = 2;
-
-            tree.Add(a);
-            tree.Add(b);
-
-            a.Add(3);
-            tree.Add(a);
-
-            Assert.AreEqual(expected, tree.Count);
-        }
-
-        [TestMethod]
-        public void SearchNulltest()
-        {
-            var tree = new Tree<List<int>>();
-            var a = new List<int>();
-            var b = new List<int>();
-            var c = new List<int>();
-
-            tree.Add(a);
-            tree.Add(b);
-
-            var result = tree.Search(c);
-
-            Assert.AreEqual(null, result);
-        }
-
-        [TestMethod]
-        public void SearchNonNulltest()
-        {
-            var tree = new Tree<List<int>>();
-            var a = new List<int>();
-            var b = new List<int>();
-            var c = new List<int>();
-
-            tree.Add(a);
-            tree.Add(b);
-            tree.Add(c);
-
-            var result = tree.Search(c);
-
-            Assert.AreNotEqual(null, result);
-        }
-
-        [TestMethod]
-        public void DeletePlusResultTest()
-        {
-            var tree = new Tree<List<int>>();
-            var a = new List<int>();
-            var b = new List<int>();
-            var c = new List<int>();
-
-            tree.Add(a);
-            tree.Add(b);
-            tree.Add(c);
-
-            var result = tree.Remove(c);
-
-            Assert.AreEqual(true, result);
-        }
-
-        [TestMethod]
-        public void DeleteMinusResultTest()
-        {
-            var tree = new Tree<List<int>>();
-            var a = new List<int>();
-            var b = new List<int>();
-            var c = new List<int>();
-            var d = new List<int>();
-
-            tree.Add(a);
-            tree.Add(b);
-            tree.Add(d);
-
-            var result = tree.Remove(c);
-
-            Assert.AreEqual(false, result);
+            Assert.AreEqual(expect, tree.Count);
         }
     }
 }
