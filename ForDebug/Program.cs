1846e43ec1b2c4068e34c163932aa83d0f6a9f80
Mikhail Bolshakov <veujin@gmail.com>
2015-07-12 16:30:05 +0300

    Added ForDebug Project

---
+++ ForDebug/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bolshakov_1;
+
+namespace ForDebug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = new Tree<List<int>>();
+            var a = new List<int>();
+            var b = new List<int>();
+            var c = new List<int>();
+            var d = new List<int>();
+
+            tree.Add(a);
+            tree.Add(b);
+            tree.Add(c);
+
+            var result = tree.Search(c);
+            var smt = 3;
+        }
+    }
+}
